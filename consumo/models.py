8a1a2a0a2efa02f00f6d087fe06c2f1637e9805d
chocopiano <37153534+chocopiano@users.noreply.github.com>
2020-05-04 17:35:06 -0400

    Add files via upload

--- consumo/models.py
+++ consumo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Ingredientes(models.Model):
     idi=models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=30, blank=False, null=False,unique=True)
+    nombre = models.CharField(max_length=255, blank=False, null=False,unique=False)
     descripcion = models.TextField()
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Ingredientes(models.Model):
 
 class Hamburguesa(models.Model):
     ide=models.AutoField(primary_key=True)
-    nombre=models.CharField(max_length=30,blank=False,null=False,unique=True)
+    nombre=models.CharField(max_length=255,blank=False,null=False,unique=False)
     precio=models.IntegerField(blank=False,null=False)
     descripcion=models.TextField()
     imagen=models.CharField(max_length=100)
